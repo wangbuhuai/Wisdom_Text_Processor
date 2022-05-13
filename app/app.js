2c75ecb9c9d3271e601b25b131e04d7222d307da
Dayu Wang <dwang@stchas.edu>
2022-05-13 09:48:13 -0500

    Completed

---
+++ app/app.js
@@ -0,0 +1,129 @@
+// Created by Dayu Wang (dwang@stchas.edu) on 2022-05-12
+
+// Last updated by Dayu Wang (dwang@stchas.edu) on 2022-05-13
+
+
+/** Restores all the buttons in the document to their initial states. */
+function restoreButtons() {
+    document.getElementById("paste").style.backgroundColor = "darkcyan";
+    document.getElementById("copy-input").style.backgroundColor = "darkcyan";
+    document.getElementById("copy-replacement").style.backgroundColor = "orangered";
+    document.getElementById("copy-view-link").style.backgroundColor = "orangered";
+    document.getElementById("copy-download-link").style.backgroundColor = "orangered";
+    document.getElementById("generate-code").style.backgroundColor = "seagreen";
+}
+
+/** Clears the access code. */
+function clearAccessCode() { document.getElementById("access-code").value = ""; }
+
+/** Shows Google Drive URLs in the document.
+    @param {string} text: a (possible) Google Drive share URL
+*/
+function showUrls(text) {
+    const urls = googleUrls(text);
+    document.getElementById("view-link").value = urls === null ? "" : urls.view;
+    document.getElementById("download-link").value = urls === null ? "" : urls.download;
+}
+
+/** Updates the document when user pastes some text to the input textbox. */
+function pasteToInput() {
+    restoreButtons();
+    clearAccessCode();
+    document.getElementById("paste").style.backgroundColor = "blue";
+    navigator.clipboard.readText().then(text => {
+        document.getElementById("input-text").value = text;
+        // Generate Google Drive URLs.
+        showUrls(text);
+        // Generate replacement text.
+        text = replaceInvalidCharacters(text);
+        document.getElementById("replacement-text").value = text;
+    });
+}
+
+/** Echoes the user input. */
+function synchronizeInput() {
+    restoreButtons();
+    clearAccessCode();
+    let text = document.getElementById("input-text").value;
+    // Generate Google Drive URLs.
+    showUrls(text);
+    // Generate replacement text.
+    text = replaceInvalidCharacters(text);
+    document.getElementById("replacement-text").value = text;
+}
+
+/** Copies the original user input text to the clipboard. */
+function copyOriginalInput() {
+    navigator.clipboard.writeText(document.getElementById("input-text").value).then(() => {
+        restoreButtons();
+        clearAccessCode();
+        document.getElementById("copy-input").style.backgroundColor = "blue";
+    });
+}
+
+/** Copies the replacement text to the clipboard. */
+function copyReplacementText() {
+    navigator.clipboard.writeText(document.getElementById("replacement-text").value).then(() => {
+        restoreButtons();
+        clearAccessCode();
+        document.getElementById("copy-replacement").style.backgroundColor = "blue";
+    });
+}
+
+/** Copies the Google Drive view URL. */
+function copyViewUrl() {
+    navigator.clipboard.writeText(document.getElementById("view-link").value).then(() => {
+        restoreButtons();
+        clearAccessCode();
+        document.getElementById("copy-view-link").style.backgroundColor = "blue";
+    });
+}
+
+/** Copies the Google Drive download URL. */
+function copyDownloadLink() {
+    navigator.clipboard.writeText(document.getElementById("download-link").value).then(() => {
+        restoreButtons();
+        clearAccessCode();
+        document.getElementById("copy-download-link").style.backgroundColor = "blue";
+    });
+}
+
+/** Generates an access code and copies it to the clipboard. */
+function copyAccessCode() {
+    document.getElementById("access-code").value = generateAccessCode();
+    navigator.clipboard.writeText(document.getElementById("access-code").value).then(() => {
+        restoreButtons();
+        document.getElementById("generate-code").style.backgroundColor = "blue";
+    });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    // Synchronize user input.
+    document.getElementById("input-text").addEventListener("keyup", function(e) {
+        if (!e.ctrlKey && e.key.toUpperCase() !== "CONTROL" && e.key.toUpperCase() !== "SHIFT" && e.key.toUpperCase() !== "ALT") {
+            synchronizeInput();
+        }
+    });
+    // Add mouse click listeners.
+    document.getElementById("paste").addEventListener("click", () => { pasteToInput(); });
+    document.getElementById("copy-input").addEventListener("click", () => { copyOriginalInput(); });
+    document.getElementById("copy-replacement").addEventListener("click", () => { copyReplacementText(); });
+    document.getElementById("copy-view-link").addEventListener("click", () => { copyViewUrl(); });
+    document.getElementById("copy-download-link").addEventListener("click", () => { copyDownloadLink(); });
+    document.getElementById("generate-code").addEventListener("click", () => { copyAccessCode(); });
+    // Add key up listeners.
+    document.onkeyup = function(e) {
+        if (e.ctrlKey && !e.shiftKey && !e.altKey && e.key.toUpperCase() === "V") { pasteToInput(); }
+        if (e.ctrlKey && e.shiftKey && e.key.toUpperCase() === "U") { copyReplacementText(); }
+        if (e.ctrlKey && e.shiftKey && e.key.toUpperCase() === "L") { copyViewUrl(); }
+        if (e.ctrlKey && e.shiftKey && e.key.toUpperCase() === "K") { copyDownloadLink(); }
+        if (e.ctrlKey && e.shiftKey && e.key.toUpperCase() === "Q") { copyOriginalInput(); }
+        if (e.ctrlKey && e.shiftKey && e.key.toUpperCase() === "?") { copyAccessCode(); }
+        if (e.key.toUpperCase() === "TAB") {
+            restoreButtons();
+            clearAccessCode();
+            document.getElementById("input-text").focus();
+            document.getElementById("input-text").select();
+        }
+    };
+});
---
+++ app/text.js
@@ -0,0 +1,53 @@
+// Created by Dayu Wang (dwang@stchas.edu) on 2022-05-12
+
+// Last updated by Dayu Wang (dwang@stchas.edu) on 2022-05-13
+
+
+/** Replaces invalid characters in a string to form a valid filename.
+    @param {string} text: a string to process
+    @returns {string}: a copy of the input string with all invalid characters replaced
+*/
+function replaceInvalidCharacters(text) {
+    // Replace whitespaces with a single underscore.
+    text = text.replace(/\s+/g, '_');
+    // Replace slash ('/') characters with a single hyphen ('-').
+    text = text.replace(/\/+/g, '-');
+    // Remove other invalid characters.
+    text = text.replace(/[<>:"/\\|?*]+/g,'');
+
+    return text;
+}
+
+/** Generates the Google Drive view URL and Google Drive download URL from a Google Drive share URL.
+    @param {string} text: a (possible) Google Drive share URL
+    @returns {Object|null}: an object containing the Google Drive view URL and Google Drive download URL;
+                            or {null} if the input text is not a valid Google Drive share URL
+*/
+function googleUrls(text) {
+    if (text.includes(String.raw`https://drive.google.com/file/d/`) && text.includes(String.raw`/view?usp=sharing`)) {
+        const documentId = text.substring(32, 65);  // Extract the Google Drive document ID.
+        return {
+            "view": String.raw`https://drive.google.com/uc?export=view&id=` + documentId,
+            "download": String.raw`https://drive.google.com/uc?export=download&id=` + documentId
+        };
+    }
+    return null;
+}
+
+/** Generates an access code.
+    @returns {string}: a "3 letters + 3 digits" access code
+*/
+function generateAccessCode() {
+    const DIGITS = ['3', '4', '7', '8'];
+    const LETTERS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'T', 'U', 'V', 'W', 'X', 'Y'];
+    let result = "";
+    for (let i = 0; i < 4; i++) {
+        const letterIndex = Math.floor(Math.random() * 21);
+        result += LETTERS[letterIndex];
+    }
+    for (let j = 0; j < 4; j++) {
+        const digitsIndex = Math.floor(Math.random() * 3);
+        result += DIGITS[digitsIndex];
+    }
+    return result;
+}
