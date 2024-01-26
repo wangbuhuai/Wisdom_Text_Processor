713e61a570eac6a2484d964dc4049acea53b502a
Dayu Wang <dwang@stchas.edu>
2024-01-26 11:16:22 -0600

    Add Canvas.  Remove Google

--- app/app.js
+++ app/app.js
@@ -1,6 +1,6 @@
 // Created by Dayu Wang (dwang@stchas.edu) on 2022-05-12
 
-// Last updated by Dayu Wang (dwang@stchas.edu) on 2024-01-22
+// Last updated by Dayu Wang (dwang@stchas.edu) on 2024-01-26
 
 
 /** Restores all the buttons in the document to their initial states. */
@@ -8,24 +8,40 @@ function restoreButtons() {
     document.getElementById("paste").style.backgroundColor = "#2c634c";
     document.getElementById("copy-input").style.backgroundColor = "#2c634c";
     document.getElementById("copy-replacement").style.backgroundColor = "#b60200";
-    document.getElementById("copy-view-link").style.backgroundColor = "#b60200";
-    document.getElementById("copy-download-link").style.backgroundColor = "#b60200";
-    document.getElementById("copy-onedrive-view-link").style.backgroundColor = "#b60200";
+    document.getElementById("copy-google-drive-view-link").style.backgroundColor = "#b60200";
+    document.getElementById("copy-google-drive-download-link").style.backgroundColor = "#b60200";
+    document.getElementById("copy-onedrive-download-link").style.backgroundColor = "#b60200";
+    document.getElementById("copy-canvas-download-link").style.backgroundColor = "#b60200";
     document.getElementById("generate-code").style.backgroundColor = "#2c634c";
 }
 
 /** Clears the access code. */
 function clearAccessCode() { document.getElementById("access-code").value = ""; }
 
-/** Shows Google Drive and OneDrive URLs in the document.
-    @param {string} text: a (possible) Google Drive share URL
+/** Shows Google Drive URLs in the document.
+    [Note] Google no longer supports the direct view and download URLs since 2024-01-02.
+    @param {string} text - a (possible) Google Drive share URL
 */
-function showUrls(text) {
+function showGoogleDriveUrls(text) {
     const urls = googleUrls(text);
-    const urls2 = onedriveUrls(text);
-    document.getElementById("view-link").value = urls === null ? "" : urls.view;
-    document.getElementById("download-link").value = urls === null ? "" : urls.download;
-    document.getElementById("onedrive-view-link").value = urls2 === null ? "" : urls2.view;
+    document.getElementById("google-drive-view-link").value = urls === null ? "" : urls.view;
+    document.getElementById("google-drive-download-link").value = urls === null ? "" : urls.download;
+}
+
+/** Shows business OneDrive direct download URL in the document.
+    @param {string} text - a (possible) business OneDrive share URL
+*/
+function showOneDriveUrl(text) {
+    const url = onedriveUrl(text);
+    document.getElementById("onedrive-download-link").value = url === null ? "" : url.download;
+}
+
+/** Shows Canvas download URL in the document.
+    @param {string} text - a (possible) Canvas preview URL
+*/
+function showCanvasUrl(text) {
+    const url = canvasUrl(text);
+    document.getElementById("canvas-download-link").value = url === null ? "" : url.download;
 }
 
 /** Updates the document when user pastes some text to the input textbox. */
@@ -36,7 +52,11 @@ function pasteToInput() {
     navigator.clipboard.readText().then(text => {
         document.getElementById("input-text").value = text;
         // Generate Google Drive URLs.
-        showUrls(text);
+        showGoogleDriveUrls(text);
+        // Generate OneDrive URL.
+        showOneDriveUrl(text);
+        // Generate Canvas URL.
+        showCanvasUrl(text);
         // Generate replacement text.
         text = replaceInvalidCharacters(text);
         document.getElementById("replacement-text").value = text;
@@ -49,7 +69,11 @@ function synchronizeInput() {
     clearAccessCode();
     let text = document.getElementById("input-text").value;
     // Generate Google Drive URLs.
-    showUrls(text);
+    showGoogleDriveUrls(text);
+    // Generate OneDrive direct download URL.
+    showOneDriveUrl(text);
+    // Generate Canvas download URL.
+    showCanvasUrl(text);
     // Generate replacement text.
     text = replaceInvalidCharacters(text);
     document.getElementById("replacement-text").value = text;
@@ -73,30 +97,43 @@ function copyReplacementText() {
     });
 }
 
-/** Copies the Google Drive view URL. */
-function copyViewUrl() {
-    navigator.clipboard.writeText(document.getElementById("view-link").value).then(() => {
+/** Copies the Google Drive view URL.
+    [Note] Google no longer supports the direct view and download URLs since 2024-01-02.
+*/
+function copyGoogleDriveViewUrl() {
+    navigator.clipboard.writeText(document.getElementById("google-drive-view-link").value).then(() => {
         restoreButtons();
         clearAccessCode();
-        document.getElementById("copy-view-link").style.backgroundColor = "blue";
+        document.getElementById("copy-google-drive-view-link").style.backgroundColor = "blue";
     });
 }
 
-/** Copies the Google Drive download URL. */
-function copyDownloadLink() {
-    navigator.clipboard.writeText(document.getElementById("download-link").value).then(() => {
+/** Copies the Google Drive download URL.
+    [Note] Google no longer supports the direct view and download URLs since 2024-01-02.
+*/
+function copyGoogleDriveDownloadUrl() {
+    navigator.clipboard.writeText(document.getElementById("google-drive-download-link").value).then(() => {
         restoreButtons();
         clearAccessCode();
-        document.getElementById("copy-download-link").style.backgroundColor = "blue";
+        document.getElementById("copy-google-drive-download-link").style.backgroundColor = "blue";
     });
 }
 
 /** Copies the OneDrive view URL. */
-function copyOneDriveViewLink() {
-    navigator.clipboard.writeText(document.getElementById("onedrive-view-link").value).then(() => {
+function copyOneDriveDownloadUrl() {
+    navigator.clipboard.writeText(document.getElementById("onedrive-download-link").value).then(() => {
         restoreButtons();
         clearAccessCode();
-        document.getElementById("copy-onedrive-view-link").style.backgroundColor = "blue";
+        document.getElementById("copy-onedrive-download-link").style.backgroundColor = "blue";
+    });
+}
+
+/** Copies the Canvas download URL. */
+function copyCanvasDownloadUrl() {
+    navigator.clipboard.writeText(document.getElementById("canvas-download-link").value).then(() => {
+        restoreButtons();
+        clearAccessCode();
+        document.getElementById("copy-canvas-download-link").style.backgroundColor = "blue";
     });
 }
 
@@ -125,18 +162,20 @@ document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("paste").addEventListener("click", () => { pasteToInput(); });
     document.getElementById("copy-input").addEventListener("click", () => { copyOriginalInput(); });
     document.getElementById("copy-replacement").addEventListener("click", () => { copyReplacementText(); });
-    document.getElementById("copy-view-link").addEventListener("click", () => { copyViewUrl(); });
-    document.getElementById("copy-download-link").addEventListener("click", () => { copyDownloadLink(); });
-    document.getElementById("copy-onedrive-view-link").addEventListener("click", () => { copyOneDriveViewLink(); });
+    document.getElementById("copy-google-drive-view-link").addEventListener("click", () => { copyGoogleDriveViewUrl(); });
+    document.getElementById("copy-google-drive-download-link").addEventListener("click", () => { copyGoogleDriveDownloadUrl(); });
+    document.getElementById("copy-onedrive-download-link").addEventListener("click", () => { copyOneDriveDownloadUrl(); });
+    document.getElementById("copy-canvas-download-link").addEventListener("click", () => { copyCanvasDownloadUrl(); });
     document.getElementById("generate-code").addEventListener("click", () => { copyAccessCode(); });
     // Add key up listeners.
     document.onkeyup = function(e) {
         if (e.ctrlKey && !e.shiftKey && !e.altKey && e.key.toUpperCase() === "V") { pasteToInput(); }
         if (e.ctrlKey && e.shiftKey && e.key.toUpperCase() === "U") { copyReplacementText(); }
-        if (e.ctrlKey && e.shiftKey && e.key.toUpperCase() === "L") { copyViewUrl(); }
-        if (e.ctrlKey && e.shiftKey && e.key.toUpperCase() === "K") { copyDownloadLink(); }
+        if (e.ctrlKey && e.shiftKey && e.key.toUpperCase() === "L") { copyGoogleDriveViewUrl(); }
+        if (e.ctrlKey && e.shiftKey && e.key.toUpperCase() === "K") { copyGoogleDriveDownloadUrl(); }
         if (e.ctrlKey && e.shiftKey && e.key.toUpperCase() === "Q") { copyOriginalInput(); }
-        if (e.ctrlKey && e.shiftKey && e.key.toUpperCase() === "H") { copyOneDriveViewLink(); }
+        if (e.ctrlKey && e.shiftKey && e.key.toUpperCase() === "H") { copyOneDriveDownloadUrl(); }
+        if (e.ctrlKey && e.shiftKey && e.key.toUpperCase() === ":") { copyCanvasDownloadUrl(); }
         if (e.ctrlKey && e.shiftKey && e.key.toUpperCase() === "?") { copyAccessCode(); }
         if (e.key.toUpperCase() === "TAB") {
             restoreButtons();
--- app/text.js
+++ app/text.js
@@ -1,11 +1,11 @@
 // Created by Dayu Wang (dwang@stchas.edu) on 2022-05-12
 
-// Last updated by Dayu Wang (dwang@stchas.edu) on 2024-01-22
+// Last updated by Dayu Wang (dwang@stchas.edu) on 2024-01-26
 
 
 /** Replaces invalid characters in a string to form a valid filename.
-    @param {string} text: a string to process
-    @returns {string}: a copy of the input string with all invalid characters replaced
+    @param {string} text - a string to process
+    @returns {string} - a copy of the input string with all invalid characters replaced
 */
 function replaceInvalidCharacters(text) {
     // Replace whitespaces with a single underscore.
@@ -14,14 +14,14 @@ function replaceInvalidCharacters(text) {
     text = text.replace(/\/+/g, '-');
     // Remove other invalid characters.
     text = text.replace(/[<>:"/\\|?*]+/g,'');
-
     return text;
 }
 
 /** Generates the Google Drive view URL and Google Drive download URL from a Google Drive share URL.
-    @param {string} text: a (possible) Google Drive share URL
-    @returns {Object|null}: an object containing the Google Drive view URL and Google Drive download URL;
-                            or {null} if the input text is not a valid Google Drive share URL
+    [Note] Google no longer supports the direct view and download URLs since 2024-01-02.
+    @param {string} text - a (possible) Google Drive share URL
+    @returns {Object|null} -  an object containing the Google Drive view URL and Google Drive download URL;
+                              or {null} if the input text is not a valid Google Drive share URL
 */
 function googleUrls(text) {
     if (text.includes(String.raw`https://drive.google.com/file/d/`) && text.includes(String.raw`/view?usp=sharing`)) {
@@ -34,33 +34,47 @@ function googleUrls(text) {
     return null;
 }
 
-/** Generates the OneDrive view URL from a OneDrive share URL.
-    @param {string} text: a (possible) OneDrive share URL
-    @returns {Object|null}: an object containing the OneDrive view URL;
-                            or {null} if the input text is not a valid OneDrive share URL
+/** Generates the OneDrive direct download URL from a business OneDrive share URL.
+    @param {string} text -  a (possible) business OneDrive share URL
+    @returns {Object|null} -  an object containing the OneDrive direct download URL;
+                              or {null} if the input text is not a valid business OneDrive share URL
 */
-function onedriveUrls(text) {
+function onedriveUrl(text) {
     if (text.includes(String.raw`my.sharepoint.com`) && text.includes(String.raw`?e=`)) {
+        return { "download": text.substring(0, text.indexOf(String.raw`?e=`)) + String.raw`?download=1` };
+    }
+    return null;
+}
+
+/** Generates the Canvas direct download URL from a Canvas preview URL.
+    @param {string} text - a (possible) Canvas preview URL
+    @returns {Object|null} - an object containing the Canvas direct download URL;
+                             or {null} if the input text is not a valid Canvas preview URL
+*/
+function canvasUrl(text) {
+    if (text.includes(String.raw`stchas.instructure.com`) && text.match(/preview=\d+$/g) !== null) {
+        const courseId = text.match(/(?<=courses\/)\d{5,}(?=\/files)/g)[0];
+        const fileId = text.match(/(?<=preview=)\d{7,}$/g)[0];
         return {
-            "view": text.substring(0, text.indexOf(String.raw`?e=`)) + String.raw`?download=1`
+            "download": String.raw`src='/courses/` + courseId + String.raw`/files/` + fileId + String.raw`/download' id='` + fileId + String.raw`'`
         };
     }
     return null;
 }
 
-/** Generates an access code.
-    @returns {string}: a "3 letters + 3 digits" access code
+/** Generates a random access code for Canvas quizzes.
+    @returns {string} - a random "4 letters + 4 digits" access code for Canvas quizzes
 */
 function generateAccessCode() {
     const DIGITS = ['3', '4', '7', '8'];
     const LETTERS = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'J', 'K', 'L', 'M', 'N', 'P', 'Q', 'R', 'T', 'U', 'V', 'W', 'X', 'Y'];
     let result = "";
     for (let i = 0; i < 4; i++) {
-        const letterIndex = Math.floor(Math.random() * 21);
+        const letterIndex = Math.floor(Math.random() * 22);
         result += LETTERS[letterIndex];
     }
     for (let j = 0; j < 4; j++) {
-        const digitsIndex = Math.floor(Math.random() * 3);
+        const digitsIndex = Math.floor(Math.random() * 4);
         result += DIGITS[digitsIndex];
     }
     return result;
